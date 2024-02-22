00e70eaed86bbeef2ead00ad24c080b3b34df965
ramen109 <aa.goodspeed@gmail.com>
2024-02-22 15:32:09 -0600

    added custom tooltips

--- src/main/java/net/Andrew/chinamod/block/custom/GuBlock.java
+++ src/main/java/net/Andrew/chinamod/block/custom/GuBlock.java
@@ -1,11 +1,19 @@
 package net.Andrew.chinamod.block.custom;
 
+import java.util.List;
+
+import javax.annotation.Nullable;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -23,5 +31,12 @@ public class GuBlock extends Block{
                 pLevel.playSound(pPlayer, pPos,SoundEvents.NOTE_BLOCK_BASEDRUM.get(), SoundSource.BLOCKS,50f,(float)random);  
                 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable BlockGetter pLevel, List<Component> pTooltip,
+            TooltipFlag pFlag) {
+        pTooltip.add(Component.literal("A drum that reverbs across the land when right-clicked"));
+        super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
     }
 }
